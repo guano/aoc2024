1012c83837cdab9a07b6ffb6d0aeaa1d39a0befc
Taylor Cowley <tcowley@secturion.com>
2024-12-10 17:20:45 -0700

    Day 4 complete

--- src/main.zig
+++ src/main.zig
@@ -105,6 +105,10 @@ pub fn main() !void {
             const day3 = @import("day3.zig");
             try day3.day3(allocator, &list, &cham);
         },
+        '4' => {
+            const day4 = @import("day4.zig");
+            try day4.day4(allocator, &list, &cham);
+        },
         else => {
             try cham.red().bold().printErr("Day {d} has not yet been implemented\n", .{day});
         },
